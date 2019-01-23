d5b4ba2035dbd40635faccb8da6f419d4381d8cc
elauser <1810653024@stud.fh-kufstein.ac.at>
2019-01-23 13:54:33 +0100

    Aufgabe2

--- src/Aufgabe1.java
+++ src/Aufgabe1.java
@@ -1,12 +1,14 @@
 import javax.swing.*;
 import java.io.*;
+import java.util.ArrayList;
+
 
 
 public class Aufgabe1 {
-    public static void main(String[]args){
-        while(true){
+    public static void main(String[] args) {
+        while (true) {
             int optionen = Integer.parseInt(JOptionPane.showInputDialog("1 = anmelden 2 = registrieren 3 = beenden"));
-            if (optionen ==1){
+            if (optionen == 1) {
                 String un = JOptionPane.showInputDialog("Geben Sie den Usernamen ein");
                 String pw = JOptionPane.showInputDialog("Geben Sie das Passwort ein");
                 try {
@@ -14,8 +16,7 @@ public class Aufgabe1 {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-            }
-            else if(optionen == 2){
+            } else if (optionen == 2) {
                 String un = JOptionPane.showInputDialog("Geben Sie den Usernamen ein");
                 String pw = JOptionPane.showInputDialog("Geben Sie das Passwort ein");
                 try {
@@ -23,33 +24,48 @@ public class Aufgabe1 {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-            }
-            else break;
+            } else break;
         }
     }
 
     public static void createUser(String un, String pw) throws IOException {
         File file = new File("C:\\Users\\User\\Desktop\\User\\" + un + ".txt");
-        if(file.exists()){
+        if (file.exists()) {
             System.out.println("Username already taken");
-        }
-        else{
+        } else {
             file.createNewFile();
             FileWriter fw = new FileWriter(file);
             BufferedWriter bw = new BufferedWriter(fw);
-            bw.write(pw);
+            bw.write(encrypt(pw));
             bw.close();
             System.out.println("User erstellt");
         }
     }
+
     public static String login(String un, String pw) throws IOException {
         File file = new File("C:\\Users\\User\\Desktop\\User\\" + un + ".txt");
-        if(!file.exists()) return "Invalid Username";
+        if (!file.exists()) return "Invalid Username";
         FileReader fr = new FileReader(file);
         BufferedReader br = new BufferedReader(fr);
         String pwr = br.readLine();
         br.close();
-        if(pwr.equals(pw)) return "Mein Geheimnis ist... Beim duschen bin ich nackt :^)";
+        if ((pwr.equals(encrypt(pw)))) return "Mein Geheimnis ist... Beim duschen bin ich nackt :^)";
         else return "Invalid Password";
     }
+
+    public static String encrypt(String pw) {
+        char[] charArray = pw.toCharArray();
+
+        char[] cryptArray = new char[charArray.length];
+
+        for (int i = 0; i < charArray.length; i++) {
+
+            int verschiebung = (charArray[i] + 20) % 128;
+            cryptArray[i] = (char) (verschiebung);
+
+        }
+        String re = new String(cryptArray);
+        return re;
+    }
 }
+
